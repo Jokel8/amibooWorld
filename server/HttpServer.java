ea10e1a7024867ea6c6a2700fe3d4f67e79eda1a
OleFriedrichSiegismu <dfgsdf@>
2024-12-12 12:28:40 +0100

    server

---
+++ server/HttpServer.java
@@ -0,0 +1,40 @@
+package server;
+
+import spieler.Spieler;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class HttpServer {
+    private ServerSocket socket;
+    private int port;
+    private ArrayList<Spieler> spieler;
+
+    public void start() {
+        try {
+            socket = new ServerSocket(port);
+            while (true) {
+                verbindungenAkzeptieren();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public HttpServer() {
+        this.port  = 8080;
+        this.spieler = new ArrayList<>();
+    }
+    private void verbindungenAkzeptieren() {
+        try {
+            Socket client = this.socket.accept();
+            if (client.isConnected()) {
+                new Thread(() -> {
+                    this.spieler.add(new Spieler(client));
+                }).start();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
